4635e8c90fab3449eab214c5cf4ed6bbf9a272cb
Fletcher De Meo <fletcher.demeo@gmail.com>
2024-04-09 07:31:53 +0800

    ttt testing updates

--- lesson_2/ttt_testing.js
+++ lesson_2/ttt_testing.js
@@ -1,11 +1,16 @@
-function joinOr(arr, delimiter = ', ', finalDelimiter = 'or ') {
+function joinOr(arr, delimiter = ', ', finalDelimiter = 'or') {
+  if (arr.length <= 2) {
+    return arr.join(` ${finalDelimiter} `);
+  }
+
   let lastInd = arr.length - 1;
   let str1 = arr.slice(0, lastInd).join(delimiter);
   let str2 = `${finalDelimiter} ${arr[lastInd]}`;
-  return `${str1} ${str2}`;
+  return `${str1}${delimiter}${str2}`;
 }
 
 // obj is the context for `joinOr`; replace it with the correct context.
+joinOr([1])                   // # => "1 or 2"
 joinOr([1, 2])                   // # => "1 or 2"
 joinOr([1, 2, 3])                // # => "1, 2, or 3"
 joinOr([1, 2, 3], '; ')          // # => "1; 2; or 3"
